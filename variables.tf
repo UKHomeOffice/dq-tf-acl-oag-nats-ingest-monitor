9cc6f3124708116a47e21955446553fc461ebd38
Jwanderema <wanderema@hotmail.co.uk>
2020-09-14 12:09:27 +0100

    fix undeclared var name

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 locals {
-  naming_suffix = "${var.pipeline_name}-${var.naming_suffix}"
+  naming_suffix = "${var.monitor_name}-${var.naming_suffix}"
   path_module   = var.path_module != "unset" ? var.path_module : path.module
 }
 
