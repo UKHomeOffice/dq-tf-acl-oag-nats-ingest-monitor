b28e904e11c2bf00bc58efdaf20bde78788e3df2
Mustafa Deerow <mdeerow@hodqadms-MacBook-Pro-4.local>
2023-12-29 16:42:55 +0000

    improve logging

--- acl_data_ingest_monitor.tf
+++ acl_data_ingest_monitor.tf
@@ -125,20 +125,17 @@ resource "aws_iam_policy" "acl_data_ingest_monitor_logging" {
   "Version": "2012-10-17",
   "Statement": [
     {
+      "Effect": "Allow",
+      "Action": "logs:CreateLogGroup",
+      "Resource": "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/*"
+    },
+    {
+      "Effect": "Allow",
       "Action": [
         "logs:CreateLogStream",
         "logs:PutLogEvents"
       ],
-      "Resource": [
-        "${aws_cloudwatch_log_group.acl_data_ingest_monitor.arn}",
-        "${aws_cloudwatch_log_group.acl_data_ingest_monitor.arn}/*"
-      ],
-      "Effect": "Allow"
-    },
-    {
-       "Action": "logs:CreateLogGroup",
-       "Resource": "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*",
-       "Effect": "Allow"
+      "Resource": "${aws_cloudwatch_log_group.acl_data_ingest_monitor.arn}:log-stream:*"
     }
   ]
 }
